3d706a791cd8aa84f72493f1e3d2602800ac9ee0
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-18 23:55:26 +0300

    refactoring code brain-even

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 import readlineSync from 'readline-sync';
 
 export const hello = () => {
-	const userName = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${userName} !`); 
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName} !`);
+  evenGame(userName);
 };
 const randomNumber = () => {
-	const number = Math.floor(Math.random() * 10000);
-	return number.toFixed(); 
-
+  const number = Math.floor(Math.random() * 10000);
+  return number.toFixed();
 };
 
-export const evenGame = () => {
-	let countAnswer = 0;
-	while (countAnswer <= 3){
-		let number = randomNumber();
-		console.log(`Question: ${number}`);		
-		const answer = readlineSync.question(`Your answer: `);
-		if (countAnswer == 3){
-			return console.log(`Congratulations, {userName}!`);		
-		}
-		if (answer == 'yes' && number%2 ==0) {
-			countAnswer++; 
-			console.log('Correct!'); 
-		} else if (answer == 'no' && number%2 !==0){
-			countAnswer++;
-			console.log('Correct!');
-		} else {
-			return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\n
-			Let's try again, {userName}!`);
-		}
-		
-	}
-	
-	  
+const question = () => {
+  const number = randomNumber();
+  console.log(`Question: ${number}`);
+  return number;
+};
+
+const answer = (num) => {
+  const answerUser = readlineSync.question('Your answer: ');
+  if ((answerUser === 'yes' && num % 2 === 0) || (answerUser === 'no' && num % 2 !== 0)) {
+    console.log('Correct!');
+    return true;
+  }
+  return false;
+};
+
+export const evenGame = (userName) => {
+  let countAnswer = 0;
+  const countRounds = 3;
+  while (countAnswer < countRounds) {
+    const num = question();
+    if (!answer(num)) {
+      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${userName}!`);
+    }
+	  countAnswer += 1;
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
