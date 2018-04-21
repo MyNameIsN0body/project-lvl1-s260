8c792217837a4bcdc8793d0ba44c9feeb9f36e20
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-22 00:50:23 +0300

    add func gcdGame()

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { evenGame } from '../games/even';
 import { calcGame } from '../games/calc';
+import { gcdGame } from '../games/gcd';
 
 calcGame();
 evenGame();
+gcdGame();
