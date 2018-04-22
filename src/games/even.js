583af6934a56a7bc100415b1933293ce91294a05
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-22 19:31:15 +0300

    fix error

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,12 @@
-import { cons } from 'hexlet-pairs';
 import { randomNumber, structureGame } from '../index';
 
 
 const task = 'Answer "yes" if number even otherwise answer "no".\n';
-//const task = cons(task1, null);
+
 
 export const evenGame = () => {
   const question = randomNumber();
   const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
-  // const answerGame = cons(question, correctAnswer);
   const answerGame = [question, correctAnswer];
   return answerGame;
 };
