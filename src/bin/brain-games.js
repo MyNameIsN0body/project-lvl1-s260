e26834f46f3ef7259f5eb00a3c0953d66e95f511
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-23 15:57:08 +0300

    add func balans

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,7 +2,9 @@
 import { evenGame } from '../games/even';
 import { calcGame } from '../games/calc';
 import { gcdGame } from '../games/gcd';
+import { balansGame } from '../games/balans';
 
 calcGame();
 evenGame();
 gcdGame();
+balansGame();
