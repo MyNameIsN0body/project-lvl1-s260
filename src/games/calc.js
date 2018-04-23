57bf66ae9475893ef2a477727ac66657bf44495f
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-23 10:47:52 +0300

    del pair

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,3 @@
-import { cons, car, cdr } from 'hexlet-pairs';
 import { structureGame } from '../index';
 
 const task = 'What is the result of the expression?';
@@ -7,41 +6,20 @@ const task = 'What is the result of the expression?';
 export const calcGame = () => {
   const indexArr = (Math.floor(Math.random() * 3));
   const symbol = ['+', '-', '*'];
-  // const num1 = (Math.floor(Math.random() * 100));
-  // const num2 = (Math.floor(Math.random() * 100));
-  const randomNum = cons((Math.floor(Math.random() * 100)), (Math.floor(Math.random() * 100)));
+  const num1 = (Math.floor(Math.random() * 100));
+  const num2 = (Math.floor(Math.random() * 100));
   let question;
   let answer;
-  // switch (symbol[indexArr]) {
-  //   case '+': {
-  //     question = `${num1} + ${num2}`;
-  //     answer = num1 + num2;
-  //     break;
-  //   }
-  //   case '-': {
-  //     question = `${num1} - ${num2}`;
-  //     answer = num1 - num2;
-  //     break;
-  //   }
-  //   case '*': {
-  //     question = `${num1} * ${num2}`;
-  //     answer = num1 * num2;
-  //     break;
-  //   } default: {
-  //     console.log('no symbol');
-  //   }
-  // }
   if (symbol[indexArr] === '+') {
-    question = `${car(randomNum)} + ${cdr(randomNum)}`;
-    answer = String(car(randomNum) + cdr(randomNum));
+    question = `${num1} + ${num2}`;
+    answer = String(num1 + num2);
   } else if (symbol[indexArr] === '-') {
-    question = `${car(randomNum)} - ${cdr(randomNum)}`;
-    answer = String(car(randomNum) - cdr(randomNum));
+    question = `${num1} - ${num2}`;
+    answer = String(num1 - num2);
   } else if (symbol[indexArr] === '*') {
-    question = `${car(randomNum)} * ${cdr(randomNum)}`;
-    answer = String(car(randomNum) * cdr(randomNum));
+    question = `${num1} * ${num2}`;
+    answer = String(num1 * num2);
   }
-  // const answerGame = cons(question, answer);
   const answerGame = [question, answer];
   return answerGame;
 };
