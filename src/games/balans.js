bebc9d30da0a3a0892c40304c570cfcbb1c01004
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-23 17:50:54 +0300

    fix code balans

--- src/games/balans.js
+++ src/games/balans.js
@@ -2,26 +2,40 @@ import { structureGame, randomNumber } from '../index';
 
 const task = 'Balance the given number.';
 
-const balanced = (arr) => {
-  const arrChar = String(arr).split('').sort();
-  let arrMax = arrChar[arrChar.length - 1];
-  let arrMin = arrChar[0];
+const balanced = (str) => {
+  const arrNum = String(str).split('').sort();
+  let arrMax = arrNum[arrNum.length - 1];
+  let arrMin = arrNum[0];
   if (arrMax - arrMin > 1) {
-    arrMax -= 1;
-    arrMin += 1;
-    const newArryChar = arrChar.slice(1, arrChar.length - 1);
-    const emptyArr = [];
-    const newArray = emptyArr.concat(arrMin, newArryChar, arrMax).join('');
+    arrMax--;
+    arrMin++;
+    const arr7 = arrNum.slice(1, arrNum.length - 1);
+    const NA = [];
+    const newArray = NA.concat(arrMin, arr7, arrMax).join('');
     return balanced(newArray);
   }
-  return arrChar.join('');
+  return arrNum.join('');
 };
+//   const arrChar = String(arr).split('').sort();
+//   let arrMax = arrChar[arrChar.length - 1];
+//   let arrMin = arrChar[0];
+//   if (arrMax - arrMin > 1) {
+//     arrMax -= 1;
+//     arrMin += 1;
+//     const newArryChar = arrChar.slice(1, arrChar.length - 1);
+//     const emptyArr = [];
+//     const newArray = emptyArr.concat(arrMin, newArryChar, arrMax).join('');
+//     return balanced(newArray);
+//   }
+//   return Number(arrChar.join(''));
+// };
 
-const balansGame = () => {
+export const balansGame = () => {
   const question = randomNumber();
-  const answer = String(balanced(question));
+  const answer = balanced(question);
   const answerGame = [question, answer];
   return answerGame;
 };
 
 export default structureGame(task, balansGame);
+
