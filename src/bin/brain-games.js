fa9541bcfe937df4a784647f52eec166b319221a
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-24 02:31:48 +0300

    add func prime

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -4,9 +4,11 @@ import { calcGame } from '../games/calc';
 import { gcdGame } from '../games/gcd';
 import { balansGame } from '../games/balans';
 import { progressionGame } from '../games/progression';
+import { primeGame } from '../games/prime';
 
 calcGame();
 evenGame();
 gcdGame();
 balansGame();
 progressionGame();
+primeGame();
