44812d06c800c63f62b590ea6ceccf9cb471a861
MyNameIsN0body <homer_xxx86@mail.ru>
2018-04-24 12:01:58 +0300

    fix func isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,22 @@
 import { structureGame } from '../index';
 
-const task = 'Is this number prime? \'yes\' or \'no\'';
+const task = 'Is this number prime?';
 
 export const primeGame = () => {
   const isPrime = (num) => {
     if (num < 2) {
-      return 'no';
+      return false;
     }
     for (let i = 2; i < num / 2; i += 1) {
       if (num % i === 0) {
-        return 'no';
+        return false;
       }
     }
-    return 'yes';
+    return true;
   };
 
   const question = Math.floor(Math.random() * 1000);
-  const answer = isPrime(question);
+  const answer = isPrime(question) ? 'yes' : 'no';
   const answerGame = [question, answer];
   return answerGame;
 };
