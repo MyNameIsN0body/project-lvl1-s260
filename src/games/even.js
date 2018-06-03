594ac1be6049f6d68886ea4a90bc51ae6cdfd35b
MyNameIsN0body <homer_xxx86@mail.ru>
2018-06-03 12:16:56 +0300

    even will render the function randomNumber1 in a separate file

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,12 @@
-import { randomNumber, structureGame } from '../index';
+import structureGame from '../index';
+import randomNumber1 from './math';
 
 
 const task = 'Answer "yes" if number even otherwise answer "no".\n';
 
 
 export const evenGame = () => {
-  const question = randomNumber();
+  const question = randomNumber1(1, 1000);
   const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
   const answerGame = [question, correctAnswer];
   return answerGame;
